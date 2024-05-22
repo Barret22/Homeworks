8b690dc56fce9d5d12ea1245fecbf3b2f06df7ea
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-22 21:35:10 +0200

    dz 6.2

---
+++ HM 6/dz 6.2.js
@@ -0,0 +1,13 @@
+
+
+function getAverageAge(users) {
+    return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+}
+
+let ivan = { name: "Іван", age: 25 };
+let petro = { name: "Петро", age: 30 };
+let mariya = { name: "Марія", age: 29 };
+
+let arr = [ ivan, petro, mariya ];
+
+alert( getAverageAge(arr) );
