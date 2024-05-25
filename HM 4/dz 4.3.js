51e13c3e3e7fb9649c996df422c55a23b2c60aff
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-25 11:14:21 +0200

    dz4.3

--- HM 4/dz 4.3.js
+++ HM 4/dz 4.3.js
@@ -1,19 +1,50 @@
 
 
-
-let uzerYears = prompt("Скільки тобі років?");
-if (uzerYears) {
-    alert(uzerYears);
-}
-else {
-    alert(
-        "Шкода, що не вказав свій вік"
-    )
-}
-let uzerCity = prompt("В якому місті ти живеш?");
-if (!uzerCity) {
-    alert("Шкода, що не захотіли вказати місце проживання!");
-}
-else {
-    alert(`Ти живеш у місті ${uzerCity}.`)
+let birthYear = prompt("Введіть ваш рік народження:");
+if (birthYear === null) {
+    alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+} else {
+    let city = prompt("В якому місті Ви живете?");
+    if (city === null) {
+        alert("Шкода, що Ви не захотіли ввести своє місто.");
+    } else {
+        let sport = prompt("Ваш улюблений вид спорту?");
+        if (sport === null) {
+            alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+        } else {
+            let currentYear = new Date().getFullYear();
+            let age = currentYear - parseInt(birthYear);
+            let cityMessage;
+            switch (city.toLowerCase()) {
+                case 'київ':
+                    cityMessage = "Ти живеш у столиці України.";
+                    break;
+                case 'вашингтон':
+                    cityMessage = "Ти живеш у столиці США.";
+                    break;
+                case 'лондон':
+                    cityMessage = "Ти живеш у столиці Великобританії.";
+                    break;
+                default:
+                    cityMessage = `Ти живеш у місті ${city}.`;
+                    break;
+            }
+            let sportMessage;
+            switch (sport.toLowerCase()) {
+                case 'футбол':
+                    sportMessage = "Круто! Хочеш стати як Ліонель Мессі?";
+                    break;
+                case 'баскетбол':
+                    sportMessage = "Круто! Хочеш стати як Майкл Джордан?";
+                    break;
+                case 'теніс':
+                    sportMessage = "Круто! Хочеш стати як Роджер Федерер?";
+                    break;
+                default:
+                    sportMessage = `Твій улюблений вид спорту - ${sport}.`;
+                    break;
+            }
+            alert(`Ваш вік: ${age} років\n${cityMessage}\n${sportMessage}`);
+        }
+    }
 }
