8c8075730b3024fa8746f100b606939b4edcedb4
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-06-03 22:05:45 +0200

    dz8.1 перездача

--- HM 8/dz8.1.js
+++ HM 8/dz8.1.js
@@ -1,19 +1,19 @@
 
 
-function Ladder() {
-    this.step = 0;
-}
-Ladder.prototype.up = function() {
-    this.step++;
-    return this;
+
+let ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        console.log(this.step);
+        return this;
+    }
 };
-Ladder.prototype.down = function() {
-    this.step--;
-    return this;
-};
-Ladder.prototype.showStep = function() {
-    console.log(this.step);
-    return this;
-};
-const ladder = new Ladder();
-ladder.up().up().down().showStep();
+ladder.up().up().up().down().showStep();
