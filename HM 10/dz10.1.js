764f0db90ceae2af36389a74224e2ee0e5340e24
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-06-08 12:34:38 +0200

    dz10.1

---
+++ HM 10/dz10.1.js
@@ -0,0 +1,16 @@
+
+
+
+const account = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: '25',
+    cityLive: 'Lviv',
+    phone: '+38746465132',
+    email: 'john@doe.com',
+};
+
+const values = Object.values(account);
+console.log(values);
+
+
