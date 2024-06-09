fe5f306a6e7cf86bbfa544dfb371670852d7b288
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-06-09 12:55:52 +0200

    dz10.2.js

--- HM 10/dz10.2.js
+++ HM 10/dz10.2.js
@@ -1,7 +1,8 @@
 
-
+const listNumbers1 = [1, 22, 3, 22, 4, 5, 6, 77, 8, 9, 77];
 const listNumbers = [1, 22, 3, 22, 4, 5, 6, 77, 8, 9, 77];
 
-const duplicateNumbers = listNumbers.filter((item, index, arr) => arr.indexOf(item) !== arr.lastIndexOf(item) && arr.indexOf(item) === index);
 
+const duplicateNumbers = listNumbers.filter((item, index, arr) => arr.indexOf(item) !== arr.lastIndexOf(item) && arr.indexOf(item) === index);
+console.log(listNumbers1);
 console.log(duplicateNumbers);
